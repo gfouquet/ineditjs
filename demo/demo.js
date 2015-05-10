b55ca7eaede7abdd9df83f97a80ab63308f070c4
gfouquet <gregory.fouquet@gmail.com>
2015-05-10 21:59:50 +0200

    Testing (native) date input with webshims polyfill - which don't properly work

--- demo/demo.js
+++ demo/demo.js
@@ -1,12 +1,19 @@
 require.config({
-  shim: {},
+  shim: {
+
+  },
   paths: {
     jquery: "../bower_components/jquery/jquery",
-    requirejs: "../bower_components/requirejs/require"
+    requirejs: "../bower_components/requirejs/require",
+    polyfiller: "../bower_components/webshim/js-webshim/minified/polyfiller"
   },
-  packages: []
+  packages: [
+
+  ]
 });
-require(["jquery", "../lib/inedit.js"], function ($) {
+require(["jquery", "polyfiller",  "../lib/inedit.js"], function ($, webshims) {
+  webshims.polyfill();
+
   $(document).on("inedit:validate", function (event, data) {
     console.log("validating", arguments);
     //var id = $()
